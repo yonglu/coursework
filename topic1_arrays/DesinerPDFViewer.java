ec69616ece3cc16b3e9e57da747e0450c6f923ed
Yong Lu <y.lu@f5.com>
2020-06-28 17:35:29 -0700

    initial checkin

--- topic1_arrays/DesinerPDFViewer.java
+++ topic1_arrays/DesinerPDFViewer.java
@@ -4,33 +4,48 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class DesinerPDFViewer {
 
+    // Complete the designerPdfViewer function below.
     static int designerPdfViewer(int[] h, String word) {
+        if (word == null || h.length != 26) {
+            return -1;
+        }
+        int len = word.length();
         int maxHeight = 0;
-        int index = 0;
-        for (int i = 0; i < word.length(); i++) {
-            // note that characters can be treated as numbers. 'b' -'a' = 1. 
-            index = word.charAt(i) - 'a'; 
-            if (h[index] > maxHeight) {
-                maxHeight = h[index];
+        for (int i = 0; i < len; i++) {
+            char c = word.charAt(i);
+            if (h[c - 'a'] > maxHeight) {
+                maxHeight = h[c - 'a'];
             }
         }
-        return maxHeight * word.length();
+        return maxHeight * len;
     }
 
-    /**
-    * HackerRank provides this code.
-    */
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
         int[] h = new int[26];
-        for(int h_i = 0; h_i < 26; h_i++){
-            h[h_i] = in.nextInt();
+
+        String[] hItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 26; i++) {
+            int hItem = Integer.parseInt(hItems[i]);
+            h[i] = hItem;
         }
-        String word = in.next();
+
+        String word = scanner.nextLine();
+
         int result = designerPdfViewer(h, word);
-        System.out.println(result);
-        in.close();
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
 }
